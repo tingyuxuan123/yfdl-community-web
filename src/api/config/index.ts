9ec8cb9f1d31c4e562526c16d38cc5caa0827d5e
tingyuxuan123 <1940203983@qq.com>
2023-01-01 01:18:46 +0800

    改接口

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -3,7 +3,8 @@ const TIME_OUT = 10000
 
 if (import.meta.env.PROD) {
   //生产环境
-  baseURL = 'http://127.0.0.1:5173/api'
+  // baseURL = 'http://127.0.0.1:5173/api'
+  baseURL = 'http://yfdl.site:7777'
 } else {
   //开发环境
   baseURL = 'http://127.0.0.1:5173'
